792371bf59d20a777c25cc5961ae2e9737411918
Apu Sikder <apusikder750@gmail.com>
2026-05-25 14:06:41 +0600

    server code

--- server/src/middleware/multer.ts
+++ server/src/middleware/multer.ts
@@ -1,9 +1,18 @@
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
+import fs from "fs";
+const uploadPath = "src/public/images";
+
+// auto create folder if not exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+  console.log("📁 Upload folder created");
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "src/public/images");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = uuidv4() + path.extname(file.originalname);
