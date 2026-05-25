6aa21388b9e5e310cb9cb37c6f162fc22018506a
Apu Sikder <apusikder750@gmail.com>
2026-05-25 11:53:47 +0600

    secure true

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://social-media-app-full-stack-mfpm.vercel.app/",
+    origin: "https://social-media-app-full-stack-mfpm.vercel.app",
     credentials: true,
   })
 );
